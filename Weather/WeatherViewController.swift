fd586291ae8d0bee26def786458b7e7f15c6cd52
ufukcanli <iletisim@ufukcanli.com>
2020-12-25 12:19:23 +0300

    Make user interface.

---
+++ Weather/WeatherViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Ufuk Canlı on 25.12.2020.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var conditionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func addLocationButtonTapped(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    @IBAction func locationButtonTapped(_ sender: UIBarButtonItem) {
+        
+    }
+}
+

8607cd8fa01bd9469c3c4131d0a65ea60738ac9f
ufukcanli <iletisim@ufukcanli.com>
2020-12-26 12:03:53 +0300

    Add better tap gesture recognizer.

--- Weather/Controller/AddCityViewController.swift
+++ Weather/Controller/AddCityViewController.swift
@@ -9,17 +9,40 @@ import UIKit
 
 class AddCityViewController: UIViewController {
     
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var statusLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         configureViewController()
     }
     
+    @objc private func dismissViewController() {
+        dismiss(animated: true, completion: nil)
+    }
+    
     private func configureViewController() {
-        let tapGesture = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissViewController))
+        tapGesture.delegate = self
         view.addGestureRecognizer(tapGesture)
 
         view.backgroundColor = UIColor.init(white: 0.3, alpha: 0.4)
+        
+        cityTextField.becomeFirstResponder()
     }
+    
+    @IBAction func searchButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+}
 
+extension AddCityViewController: UIGestureRecognizerDelegate {
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        return touch.view == self.view
+    }
 }

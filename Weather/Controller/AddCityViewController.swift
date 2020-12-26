3b5b0538fb70703559aad57370a3e0263690e810
ufukcanli <iletisim@ufukcanli.com>
2020-12-26 12:59:35 +0300

    Add basic form validation.

--- Weather/Controller/AddCityViewController.swift
+++ Weather/Controller/AddCityViewController.swift
@@ -20,8 +20,30 @@ class AddCityViewController: UIViewController {
         configureViewController()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        cityTextField.becomeFirstResponder()
+    }
+    
     @objc private func dismissViewController() {
         dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func searchButtonTapped(_ sender: UIButton) {
+        statusLabel.isHidden = true
+        guard let query = cityTextField.text, !query.isEmpty else { return }
+        searchForCity(query: query)
+        showSearchError(text: "City cannot be empty. Please try again!")
+    }
+
+    private func searchForCity(query: String) {
+        
+    }
+    
+    private func showSearchError(text: String) {
+        statusLabel.isHidden = false
+        statusLabel.textColor = .systemRed
+        statusLabel.text = text
     }
     
     private func configureViewController() {
@@ -31,13 +53,9 @@ class AddCityViewController: UIViewController {
 
         view.backgroundColor = UIColor.init(white: 0.3, alpha: 0.4)
         
-        cityTextField.becomeFirstResponder()
+        statusLabel.isHidden = true
     }
-    
-    @IBAction func searchButtonTapped(_ sender: UIButton) {
-        
-    }
-    
+
 }
 
 extension AddCityViewController: UIGestureRecognizerDelegate {
